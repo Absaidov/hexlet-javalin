7946e833fa689d569140181d8168f6fbabb6ad5c
Absaidov <dabsaidov@bk.ru>
2023-11-13 19:13:41 +0300

    9 lesson web ended, style

--- HexletJavalin/jte-classes/gg/jte/generated/ondemand/courses/JtecourseGenerated.java
+++ HexletJavalin/jte-classes/gg/jte/generated/ondemand/courses/JtecourseGenerated.java
@@ -2,15 +2,15 @@ package gg.jte.generated.ondemand.courses;
 import exercise.dto.courses.CoursePage;
 public final class JtecourseGenerated {
 	public static final String JTE_NAME = "courses/course.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,9,9,9,9,10,10,10,13};
+	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,16,16,16,16,17,17,17,21};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, CoursePage page) {
-		jteOutput.writeContent("\n<html>\n    <head>\n        <title>Хекслет</title>\n    </head>\n    <body>\n        <main>\n            <h1>");
-		jteOutput.setContext("h1", null);
+		jteOutput.writeContent("\n<html lang=\"en\">\n    <head>\n        <meta charset=\"utf-8\" />\n        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n        <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css\"\n              rel=\"stylesheet\"\n              integrity=\"sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We\"\n              crossorigin=\"anonymous\">\n        <title>Hello Hexlet!</title>\n    </head>\n    <body>\n        <div class=\"mx-auto p-4 py-md-5\">\n            <main>\n                <h2>");
+		jteOutput.setContext("h2", null);
 		jteOutput.writeUserContent(page.getCourse().getName());
-		jteOutput.writeContent("</h1>\n            <p>");
-		jteOutput.setContext("p", null);
+		jteOutput.writeContent("</h2>\n                <div>$");
+		jteOutput.setContext("div", null);
 		jteOutput.writeUserContent(page.getCourse().getDescription());
-		jteOutput.writeContent("</p>\n        </main>\n    </body>\n</html>");
+		jteOutput.writeContent("</div>\n            </main>\n        </div>\n    </body>\n</html>");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		CoursePage page = (CoursePage)params.get("page");
--- HexletJavalin/jte-classes/gg/jte/generated/ondemand/courses/JtecoursesGenerated.java
+++ HexletJavalin/jte-classes/gg/jte/generated/ondemand/courses/JtecoursesGenerated.java
@@ -2,29 +2,49 @@ package gg.jte.generated.ondemand.courses;
 import exercise.dto.courses.CoursesPage;
 public final class JtecoursesGenerated {
 	public static final String JTE_NAME = "courses/courses.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,9,9,9,11,11,12,12,14,14,14,14,14,14,14,15,15,15,17,17,18,18,20};
+	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,3,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,23,41,42,43,44,44,47,47,47,50,50,50,50,50,50,50,50,50,50,53,53,58};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, CoursesPage page) {
-		jteOutput.writeContent("\n<html>\n    <head>\n        <title>Хекслет</title>\n    </head>\n    <body>\n        <h1>HEADER</h1>\n        ");
-		if (page.getCourses().isEmpty()) {
-			jteOutput.writeContent("\n            <p>Пока не добавлено ни одного курса</p>\n        ");
-		} else {
-			jteOutput.writeContent("\n            ");
-			for (var course : page.getCourses()) {
-				jteOutput.writeContent("\n                <div>\n                    <h2><a href=\"/courses/");
-				jteOutput.setContext("a", "href");
-				jteOutput.writeUserContent(course.getId());
-				jteOutput.setContext("a", null);
-				jteOutput.writeContent("\">");
-				jteOutput.setContext("a", null);
-				jteOutput.writeUserContent(course.getName());
-				jteOutput.writeContent("</a></h2>\n                    <p>");
-				jteOutput.setContext("p", null);
-				jteOutput.writeUserContent(course.getDescription());
-				jteOutput.writeContent("</p>\n                </div>\n            ");
-			}
-			jteOutput.writeContent("\n        ");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n\n<html lang=\"en\">\n<head>\n    <meta charset=\"utf-8\" />\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css\"\n          rel=\"stylesheet\"\n          integrity=\"sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We\"\n          crossorigin=\"anonymous\">\n    <title>Hello Hexlet!</title>\n</head>\n    <body>\n\n        <div class=\"mx-auto p-4 py-md-5\">\n            <main>\n                <h1>Курсы</h1>\n                <table class=\"table table-striped\">\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\n                        ");
+		for (var course : page.getCourses()) {
+			jteOutput.writeContent("\n                            <tr>\n                                <td>\n                                    ");
+			jteOutput.setContext("td", null);
+			jteOutput.writeUserContent(course.getId());
+			jteOutput.writeContent("\n                                </td>\n                                <td>\n                                    <a href=\"/courses/");
+			jteOutput.setContext("a", "href");
+			jteOutput.writeUserContent(course.getId());
+			jteOutput.setContext("a", null);
+			jteOutput.writeContent("\">");
+			jteOutput.setContext("a", null);
+			jteOutput.writeUserContent(course.getName());
+			jteOutput.writeContent(" ");
+			jteOutput.setContext("a", null);
+			jteOutput.writeUserContent(course.getDescription());
+			jteOutput.writeContent("</a>\n                                </td>\n                            </tr>\n                        ");
 		}
-		jteOutput.writeContent("\n    </body>\n</html>");
+		jteOutput.writeContent("\n                </table>\n            </main>\n        </div>\n    </body>\n</html>");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		CoursesPage page = (CoursesPage)params.get("page");
